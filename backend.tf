4f71fbd5642e8be8f8dcdc490b3b2cff97cb82dd
yeipis <juanpablo.fernandezdelatorre04@gmail.com>
2026-08-26 05:23:10 +0200

    fix(backend): allow dynamic endpoint configuration via backend-config

--- backend.tf
+++ backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket                      = "terraform-state"
     key                         = "cloudflare-infra/terraform.tfstate"
     region                      = "auto"
-    endpoint                    = "https://<CLOUDFLARE_ACCOUNT_ID>.r2.cloudflarestorage.com"
     skip_credentials_validation = true
     skip_region_validation      = true
     skip_requesting_account_id  = true
